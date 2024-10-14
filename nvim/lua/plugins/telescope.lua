8677c69caef41ec5e91bf60d50b6b7f64b1a9c75
mehdiyamin <mehdiyamin@gmail.com>
2024-10-14 09:12:20 +0800

    Configured Neovim up to debuggers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,4 @@
-return {
+return {{
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = {'nvim-lua/plenary.nvim'},
@@ -7,5 +7,15 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
     end
+}, {
+    'nvim-telescope/telescope-ui-select.nvim',
 
-}
+    config = function()
+        require("telescope").setup {
+            extensions = {
+                ["ui-select"] = {require("telescope.themes").get_dropdown {}}
+            }
+        }
+        require("telescope").load_extension("ui-select")
+    end
+}}
