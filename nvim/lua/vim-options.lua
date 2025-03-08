5c7dcfe28b40954d388372f88a41da81f35db075
Mehdi <mehdiyamin@gmail.com>
2025-03-08 23:03:34 +0400

    Removed not-synced directories

--- nvim/lua/vim-options.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
--- Set relative line number
-vim.cmd("set relativenumber")
-vim.cmd("set number")
