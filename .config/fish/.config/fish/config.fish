7e45ccb38c4d8f9401aaf6f47c90c9a04dddac1e
Mehdi <mehdiyamin@gmail.com>
2026-06-02 22:02:00 +0400

    Added fish config

---
+++ .config/fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+# ~/.config/fish/config.fish
+
+# Keep this file minimal.
+# Most OMF-specific config lives in ~/.config/omf/init.fish.
+
+if status is-interactive
+    # Disable the default Fish greeting.
+    set -g fish_greeting
+end
---
+++ .config/fish/.config/fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,4 @@
+# ~/.config/fish/functions/fish_mode_prompt.fish
+
+function fish_mode_prompt
+end
